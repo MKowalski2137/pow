de72c20dc97978026960ca83d245732361b12fd5
10373934u <markowgithub@gmail.com>
2025-04-04 12:28:19 +0200

    burd.jpg

--- app/src/main/java/com/example/powtorzenie/MainActivity.java
+++ app/src/main/java/com/example/powtorzenie/MainActivity.java
@@ -1,38 +1,55 @@
 package com.example.powtorzenie;
 
-import android.Manifest;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.core.graphics.BitmapCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {       //zrobienie klasy main
 
-    private static final String CHANNEL_ID = "my_channel_id";
-    private static int ID = 0;
+    private static int ID = 0;      //ustawia ID powiadomienia które nie może być takie samo w przeciwnym razie powiadomienia będą się zmieniać zamiast tworzyć nowe
 
     @Override
-    protected void onCreate(Bundle SavedInstanceState){
-        super.onCreate(SavedInstanceState);
-        setContentView(R.layout.activity_main);
-        createNotificationChannel();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);     //robi aktywność
 
-        Button powiadomienie= findViewById(R.id.powiadomienie);
-        powiadomienie.setOnClickListener(view -> {
-
+        Button button = findViewById(R.id.powiadomienie);
+        button.setOnClickListener(v -> {
+                    pomocnik.wyslij(ID,
+                            this, "Powiadomienie", "To jest zwykłe powiadomienie", 0, 0);       //znajduje id powiadomienia z pomocnik i używa funkcji wyslij z pomocnik żeby wysłać wiadomość
+                    ID++;
+                }
+        );
+        Button button1 = findViewById(R.id.dlugie);     //znajduje id powiadomienia z pomocnik i używa funkcji wyslij z pomocnik żeby wysłać dużo tekstu
+        button1.setOnClickListener(view -> {
+                    pomocnik.wyslij(ID,
+                            this, "Długie powiadomienie",
+                            "Tu z winnicy miłości niedojrzałe grona\n" +
+                                    "Wzięto na stół Allaha; tu perełki Wschodu³⁴,\n" +
+                                    "Grób, Kobieta, Niewola,\n" +
+                                    "Religia\n" +
+                                    "Z morza uciech i szczęścia, porwała za młodu\n" +
+                                    "Truna³⁵, koncha wieczności, do mrocznego łona.\n" +
+                                    "Skryła గe niepamięci i czasu zasłona;\n" +
+                                    "Nad niemi turban zimny³⁶ błyszczy śród ogrodu,\n" +
+                                    "Przemĳanie\n" +
+                                    "Jak buńczuk woగska cieniów, i ledwie u spodu\n" +
+                                    "Zostały dłonią giaura³⁷ wyryte imiona.\n" +
+                                    "O wy, róże edeńskie! u czystości stoku³⁸\n" +
+                                    "Odkwitnęły dni³⁹ wasze pod wstydu liściami⁴⁰,\n" +
+                                    "Na wieki zataగone niewiernemu oku.\n" +
+                                    "Teraz grób wasz spoగrzenie cuǳoziemca plami⁴¹, —\n" +
+                                    "Pozwalam mu, — daruగesz, o wielki Proroku!\n" +
+                                    "On గeden z cuǳoziemców poglądał ze łzami.", 1, 0);
+                    ID++;
+                }
+        );
+        Button button2 = findViewById(R.id.obraz);
+        button2.setOnClickListener(view -> {
+            pomocnik.wyslij (ID,
+                    this, "obraz powiadomienie", "ptak", 2, R.drawable.burd);       //znajduje id powiadomienia z pomocnik i używa funkcji wyslij z pomocnik żeby wysłać obraz
+            ID++;
         });
+
     }
 }
--- app/src/main/java/com/example/powtorzenie/pomocnik.java
+++ app/src/main/java/com/example/powtorzenie/pomocnik.java
@@ -1,6 +1,5 @@
 package com.example.powtorzenie;
 
-import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
@@ -16,15 +15,52 @@ import androidx.core.content.ContextCompat;
 
 public class pomocnik {
     private static final String CHANNEL_ID = "default_channel";
-    private static final String CHANNEL_NAME = "Kanał powiadomień";
+    private static final String CHANNEL_NAME = "Kanał powiadomień";     //zmienne ważne do kanału powiadomień
 
 
     public static void wyslij(int NotificationID, AppCompatActivity activity,String title,String message, int styleType,int LargeIconResID){
-        if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.TIRAMISU){     //sprawdza wersję uządzenia
+        if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.TIRAMISU){     //sprawdza czy wersja android jest wyższa od Tiramisu(33)
             if(ContextCompat.checkSelfPermission(activity, android.Manifest.permission.POST_NOTIFICATIONS)!= PackageManager.PERMISSION_GRANTED){        //sprawdza czy jest zgoda na wyyłanie powadomień
-                ActivityCompat.requestPermissions(activity, new String [] {android.Manifest.permission.POST_NOTIFICATIONS}, 100);
+                ActivityCompat.requestPermissions(activity, new String [] {android.Manifest.permission.POST_NOTIFICATIONS}, 100);       //prosi o pozwolenie na wysyłanie powiadomień jeśli takiego nie ma
                 return;
             }
+        };
+        NotificationManager menager= (NotificationManager)
+                activity.getSystemService(Context.NOTIFICATION_SERVICE);        //bierze dostęp do usługi powiadomień
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){        //sprawdza czy wersja android jest wyższa od O
+            NotificationChannel kanal= new NotificationChannel(CHANNEL_ID,CHANNEL_NAME,menager.IMPORTANCE_HIGH);        //robi kanał powiadomień zawierający menadżera powiadomień, channel id,name oraz wysoką ważność powiadomienia
+            menager.createNotificationChannel(kanal);       //inicjalizuje kanał powiadomień
+
         }
+        NotificationCompat.Builder budowniczy =     //robi buildera który jest używany do tworzenia powiadomień
+                new NotificationCompat.Builder(activity, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.burd)
+                        .setContentTitle(title)
+                        .setContentText(message)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setAutoCancel(true);
+
+
+
+        switch (styleType) {
+            case 1:     //robi zwykłą wiadomość
+                budowniczy.setStyle(new NotificationCompat.BigTextStyle().bigText(message));
+                break;
+            case 2:     //robi wiadomość z obrazem
+                if (LargeIconResID != 0) {
+                    Bitmap largeIcon = BitmapFactory.decodeResource(activity.getResources(), LargeIconResID);
+                    budowniczy.setLargeIcon(largeIcon);
+                    budowniczy.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(largeIcon).setBigContentTitle(title));
+                }
+                break;
+            case 3:     //robi długą wiadomość
+                NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+                inboxStyle.addLine(message);
+                inboxStyle.addLine("Dodatkowa linia tekstu");
+                budowniczy.setStyle(inboxStyle);
+                break;
+        }
+        menager.notify(NotificationID, budowniczy.build());
     }
 }
+
